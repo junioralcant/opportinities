69fec609d211ff2ec589ff3dd4b8233bf88ff065
junioralcant <junioralcant111@gmail.com>
2024-02-14 18:16:19 -0300

    feat: create database connection

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 var (
 	db     *gorm.DB
@@ -8,7 +12,18 @@ var (
 )
 
 func Init() error {
+	var err error
+
+	db, err = InicializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("database initialization error: %v", err)
+	}
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
