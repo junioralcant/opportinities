197abf8e88711012f0abb75f44494980b5158a4b
junioralcant <junioralcant111@gmail.com>
2024-02-14 17:36:59 -0300

    feat: create logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/junioralcant/opportinities.git/router"
+import (
+	"github.com/junioralcant/opportinities.git/config"
+	"github.com/junioralcant/opportinities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
