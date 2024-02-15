c59fc8feca61c71b8710bca3e905bd09c4d78c7a
junioralcant <junioralcant111@gmail.com>
2024-02-15 17:19:31 -0300

    feat: create listOpening logic

--- controllers/listOpeningController.go
+++ controllers/listOpeningController.go
@@ -1,11 +1,20 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/junioralcant/opportinities.git/schemas"
 )
 
 func ListOpeningController(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "List World!"})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error in list openings: %v", err))
+		return
+	}
+
+	sendSuccess(ctx, "list-opening", openings)
 }
