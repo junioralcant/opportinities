794ee1441932e352fed085c866b2b9195f4053a2
junioralcant <junioralcant111@gmail.com>
2024-02-15 17:00:51 -0300

    feat: create createOpening logic

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,15 +1,16 @@
 package controllers
 
 import (
+	"github.com/junioralcant/opportinities.git/config"
 	"gorm.io/gorm"
 )
 
-type Opening struct {
-	gorm.Model
-	Role     string
-	Company  string
-	Location string
-	Remote   bool
-	Link     string
-	Salary   int64
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeController() {
+	logger = config.GetLogger("controllers")
+	db = config.GetSQLite()
 }
--- controllers/createOpeningController.go
+++ controllers/createOpeningController.go
@@ -4,8 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/junioralcant/opportinities.git/schemas"
 )
 
 func CreateOpeningController(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Post World!"})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error validating request: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	createOpening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&createOpening).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", createOpening)
 }
