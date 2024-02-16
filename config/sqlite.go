41b61b2649fdfe0bd8b7fe839446ae849d7289b4
junioralcant <junioralcant111@gmail.com>
2024-02-16 11:04:53 -0300

    feat: config postgres

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,35 +1,17 @@
 package config
 
 import (
-	"os"
-
 	"github.com/junioralcant/opportinities.git/schemas"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InicializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	dbPath := "./db/main.db"
 
-	_, err := os.Stat(dbPath)
-	if os.IsNotExist(err) {
-		logger.Info("database not found, creating...")
+	dns := "host=localhost user=postgres password=toor dbname=opportunities port=5432 sslmode=disable"
 
-		err = os.MkdirAll("./db", os.ModePerm)
-		if err != nil {
-			return nil, err
-		}
-
-		file, err := os.Create(dbPath)
-		if err != nil {
-			return nil, err
-		}
-
-		file.Close()
-	}
-
-	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 
 	if err != nil {
 		logger.Errorf("sqlite initialization error: %v", err)
